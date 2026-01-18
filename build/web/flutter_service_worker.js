0ed069b6268789837f61d4d1ade4a207b9cd1277
Sameh Alnatsheh <natsheh_s10@hotmail.com>
2026-01-18 21:21:12 +0300

    rebuild: update web assets

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "21a00ee2b5e9c5e87022cc3c23a334b5
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "7d97a5576ea75a90dcc8c5d0017d72c3",
+"flutter_bootstrap.js": "a1b4008351a0d6f886bd4f79854c0569",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
