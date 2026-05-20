3fc1f8c6bceabe80ce8eed1902c2251a3fd3c068
Sameh Alnatsheh <natsheh_s10@hotmail.com>
2026-05-20 13:27:15 +0300

    Fix web build syntax error in weekly werd reports screen

--- lib/screens/weekly_werd_reports_screen.dart
+++ lib/screens/weekly_werd_reports_screen.dart
@@ -305,7 +305,7 @@ class _WeeklyWerdReportsScreenState extends State<WeeklyWerdReportsScreen> {
                         ),
                       );
                     },
-                  ),
+                  );
           },
         ),
       ),
